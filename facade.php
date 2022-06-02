c4b5fd4b08a6eb6f6dc541a33a58723068a75ae2
PiotrMigdal <PiotrMigdal@users.noreply.github.com>
2022-06-02 17:29:08 +0100

    Facade usage

--- facade.php
+++ facade.php
@@ -15,14 +15,26 @@ class Logger
 {
 }
 
-// We have to do all these things for each template ID
+
+class TemplateFacade {
+    public function serveAndLog($id, $msg = '')
+    {
+        // We have to do all these things for each template ID
+        $db = new Database();
+        $data = $db->getData($id);
+
+        $logger = new Logger();
+        $logger->log($msg);
+
+        $template = new Template();
+        $template->serve();
+    }
+}
+
 $id = $_GET['id']; // i.e. 23
 
-$db = new Database();
-$data = $db->getData($id);
+// We just call facade so all is called at once
+$page = new TemplateFacade();
+$page->serveAndLog($id, "Accessing page with ID {$id} by user");
 
-$logger = new Logger();
-$logger->log("Accessing page with ID {$id} by user");
-
-$template = new Template();
-$template->serve();
+// Facade seems to be against single responsibility principle...
